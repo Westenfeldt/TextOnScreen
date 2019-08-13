d016a97733f811cb422090aea6b11e0491f7e9b6
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-13 12:56:44 +0200

    Add project files.

---
+++ TextOnScreen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TextOnScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alder = 16;
+            double penge = 2345.35;
+            DateTime dt = new DateTime(2020, 7, 20);
+
+            Console.Write("Jeg er {0} år og har {1} kr. i banken. Jeg går i 9 klasse og er starte på Tec Ballerup den {2}", alder, penge, dt);
+
+
+        }
+    }
+}
