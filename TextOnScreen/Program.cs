24d915ac1f89e8292951c86bda67562ca5e33d8e
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-13 13:03:13 +0200

    Tiden er fikset

--- TextOnScreen/Program.cs
+++ TextOnScreen/Program.cs
@@ -8,7 +8,7 @@ namespace TextOnScreen
         {
             int alder = 16;
             double penge = 2345.35;
-            DateTime dt = new DateTime(2020, 7, 20);
+            DateTime dt = new DateTime(2020, 7, 20, 9,0,0);
 
             Console.Write("Jeg er {0} år og har {1} kr. i banken. Jeg går i 9 klasse og er starte på Tec Ballerup den {2}", alder, penge, dt);
 
